d916ab67270b396860db98175d99906e9d7a39b2
Xalsar <xavicarreragimbert@gmail.com>
2020-10-28 19:45:19 +0100

    Create hidden card Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,16 +2,24 @@ import React from 'react'
 import classes from './Card.module.css'
 
 const Card = (props) => {
-    return <div 
-    className={classes.card}
-    style={{
-        backgroundColor: `var(${props.color})`,
-        opacity: props.active ? 0.3 : 1
-    }}
-    onClick={props.active ? null : props.click}
-    >
-        {props.active} 
-    </div>
+    if (props.hidden) {
+        return <div
+            className={[classes.card, classes.hidden].join(' ')}
+            onClick={props.click}
+        >
+            <h2>?</h2>
+        </div>
+
+    } else {
+        return <div
+            className={classes.card}
+            style={{
+                backgroundColor: `var(${props.color})`,
+                opacity: props.active ? 0.3 : 1
+            }}
+        >
+        </div>
+    }
 }
 
 export default Card
