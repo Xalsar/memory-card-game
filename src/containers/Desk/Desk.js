f42b7ac518f6f146cb4c9738f92f70acb9c63422
Xalsar <xavicarreragimbert@gmail.com>
2020-10-28 18:26:36 +0100

    Add timer to Desk

--- src/containers/Desk/Desk.js
+++ src/containers/Desk/Desk.js
@@ -1,25 +1,31 @@
 import React, { useEffect } from 'react'
 import Card from '../../components/Card/Card'
+import Timer from '../../components/Timer/Timer'
 import { connect } from 'react-redux'
 import classes from './Desk.module.css'
 
 const Desk = (props) => {
     const shuffle = props.shuffle
-    
+
     useEffect(() => {
         shuffle()
     }, [shuffle])
 
     return (
         <div className={classes.desk}>
-            {props.deck.map((card) => (
-                <Card
-                    key={card.id}
-                    color={card.color}
-                    click={() => props.pick(card.id)}
-                    active={card.found}
-                />
-            ))}
+            <div className={classes.header}>
+                <Timer />
+            </div>
+            <div className={classes.cardsContainer}>
+                {props.deck.map((card) => (
+                    <Card
+                        key={card.id}
+                        color={card.color}
+                        click={() => props.pick(card.id)}
+                        active={card.found}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
