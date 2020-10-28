b217884a83ad1551d65d427ecaaae7a5253260eb
Xalsar <xavicarreragimbert@gmail.com>
2020-10-28 14:03:55 +0100

    Remove default code App

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
-	const [message, setMessage] = useState("Loading...")
-	
-	useEffect(() => {
-		fetch('http://localhost:8080/test/1')
-		.then(res => {
-			if(res.status === 200){
-				return res.json()
-			} else {
-				Promise.reject()
-			}
-		})
-		.then(test => {
-			setMessage(test.message);
-		})
-		.catch(() => {
-			setMessage("Something is wrong...")
-		})
-	}, [])
-	
+
   return (
 		<div className={"App"}>
 			<h1>React is Working</h1>
-			<p>{message}</p>
 		</div>
   );
 }
