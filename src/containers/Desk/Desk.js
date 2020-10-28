4063ce1fd00da4e323a7379a0846b5ac003a3ca0
Xalsar <xavicarreragimbert@gmail.com>
2020-10-28 15:01:07 +0100

    Display and shuffle cards Desk

--- src/containers/Desk/Desk.js
+++ src/containers/Desk/Desk.js
@@ -3,16 +3,58 @@ import Card from '../../components/Card/Card'
 import classes from './Desk.module.css'
 
 const Desk = () => {
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    const deck = [
+        {
+            value: 1,
+            color: '--light-orange'
+        },
+        {
+            value: 1,
+            color: '--light-orange'
+        },
+        {
+            value: 2,
+            color: '--light-pink'
+        },
+        {
+            value: 2,
+            color: '--light-pink'
+        },
+        {
+            value: 3,
+            color: '--light-blue'
+        },
+        {
+            value: 3,
+            color: '--light-blue'
+        },
+        {
+            value: 4,
+            color: '--light-green'
+        },
+        {
+            value: 4,
+            color: '--light-green'
+        }
+    ]
+
+    shuffleArray(deck)
+
     return (
         <container className={classes.desk}>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/> 
-            <Card/> 
+            {deck.map((card, id) => (
+                <Card
+                    key={id}
+                    color={card.color}
+                />
+            ))}
         </container>
     )
 }
