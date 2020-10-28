9e59b34b667d1a922f7af36aa6ef3bcd2a73a52a
Xalsar <xavicarreragimbert@gmail.com>
2020-10-28 15:00:55 +0100

    Get color from props Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import classes from './Card.module.css'
 
-const Card = (props) => (
-    <div className={classes.card}>
+const Card = (props) => {
+    return <div 
+    className={classes.card}
+    style={{
+        backgroundColor: `var(${props.color})`
+    }}
+    >
     </div>
-)
+}
 
 export default Card
