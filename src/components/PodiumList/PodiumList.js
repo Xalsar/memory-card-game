0ec9e70bdf07794238811644349bc277a74e879e
Xalsar <xavicarreragimbert@gmail.com>
2020-10-29 11:38:40 +0100

    Debug PodiumList

--- src/components/PodiumList/PodiumList.js
+++ src/components/PodiumList/PodiumList.js
@@ -3,12 +3,12 @@ import classes from './PodiumList.module.css'
 
 const PodiumList = (props) => (
     <ul className={classes.list}>
-        {props.items.map((item, id) => {
+        {props.items.map((item, id) => (
             <li>
                 <span className={classes.name}>{item.name}</span>
                 <span className={classes.time}>({item.time})</span>
             </li>
-        })}
+        ))}
     </ul>
 )
 
