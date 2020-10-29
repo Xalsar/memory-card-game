d41bb7786534a5f3e3d33eb1b802e72ecdb6bb63
Xalsar <xavicarreragimbert@gmail.com>
2020-10-29 10:57:38 +0100

    Add list, title and time to Resume

--- src/containers/Resume/Resume.js
+++ src/containers/Resume/Resume.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import PodiumList from '../../components/PodiumList/PodiumList'
 import classes from './Resume.module.css'
 
 const Resume = () => {
     return <div className={classes.container}>
-        <h1>Buenas!</h1>
+        <h2 className={classes.title}>
+            Congratulations you have finished the game with a supertime of:
+        </h2>
+        <h3 className={classes.time}>
+            00:20
+        </h3>
+        <PodiumList
+            items={[
+                {
+                    name: "Morris Kline",
+                    time: "00:15"
+                },
+                {
+                    name: "Bruce wayne",
+                    time: "00:15"
+                },
+                {
+                    name: "HP Lovecraft",
+                    time: "00:15"
+                },
+                {
+                    name: "Ray Bradburry",
+                    time: "00:15"
+                },
+                {
+                    name: "Phillip K Dick",
+                    time: "00:15"
+                },
+                {
+                    name: "Robert E. Howard",
+                    time: "00:15"
+                }
+            ]}
+        />
     </div>
 }
 
