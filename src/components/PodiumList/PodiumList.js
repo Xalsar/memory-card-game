8f0dfa52e87b73c51b1249c7bd681c9483d98084
Xalsar <xavicarreragimbert@gmail.com>
2020-10-29 10:55:47 +0100

    Create PodiumList

---
+++ src/components/PodiumList/PodiumList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import classes from './PodiumList.module.css'
+
+const PodiumList = (props) => (
+    <ul className={classes.list}>
+        {props.items.map((item, id) => {
+            <li>
+                <span className={classes.name}>{item.name}</span>
+                <span className={classes.time}>({item.time})</span>
+            </li>
+        })}
+    </ul>
+)
+
+export default PodiumList
