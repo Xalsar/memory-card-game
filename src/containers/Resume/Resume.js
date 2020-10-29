4aabe73eab224d972ecc4a0152b8ed5b667051f1
Xalsar <xavicarreragimbert@gmail.com>
2020-10-29 09:32:54 +0100

    Create Resume container

---
+++ src/containers/Resume/Resume.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Resume.module.css'
+
+const Resume = () => {
+    return <div className={classes.container}>
+        <h1>Buenas!</h1>
+    </div>
+}
+
+export default Resume
