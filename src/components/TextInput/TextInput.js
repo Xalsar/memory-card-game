49fd0507030a7cb2bb392c77d2cf15502d894987
Xalsar <xavicarreragimbert@gmail.com>
2020-10-29 22:44:51 +0100

    Create TextInput

---
+++ src/components/TextInput/TextInput.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './TextInput.module.css'
+
+const TextInput = (props) => (
+    <input
+        className={classes.input}
+        type="text"
+    />
+)
+
+export default TextInput
