19909ff437587be8e60c3d5eadea8c91a777ecba
Xalsar <xavicarreragimbert@gmail.com>
2020-10-29 09:33:00 +0100

    Add Routing to App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
 import './App.css';
 import Desk from './containers/Desk/Desk'
+import Resume from './containers/Resume/Resume'
 
 function App() {
 
-  return (
+	return (
 		<div className={"App"}>
-			<Desk/>
+			<Switch>
+				<Route path={"/game"} component={Desk} exact />
+				<Route path={"/resume"} component={Resume} exact />
+			</Switch>
 		</div>
-  );
+	);
 }
 
 export default App;
