cc3647857e53a707078403eb4c35d53df7f2c35b
Xalsar <xavicarreragimbert@gmail.com>
2020-10-29 19:09:20 +0100

    Add key to PodiumList items

--- src/components/PodiumList/PodiumList.js
+++ src/components/PodiumList/PodiumList.js
@@ -4,7 +4,7 @@ import classes from './PodiumList.module.css'
 const PodiumList = (props) => (
     <ul className={classes.list}>
         {props.items.map((item, id) => (
-            <li>
+            <li key={id}>
                 <span className={classes.name}>{item.name}</span>
                 <span className={classes.time}>({item.time})</span>
             </li>
