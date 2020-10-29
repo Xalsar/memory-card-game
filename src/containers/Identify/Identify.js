1f65ee47f32af4b8d96fed5e7b43e5cedea98842
Xalsar <xavicarreragimbert@gmail.com>
2020-10-30 00:10:45 +0100

    Add redux and error handling to Identify

--- src/containers/Identify/Identify.js
+++ src/containers/Identify/Identify.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { useHistory } from "react-router-dom"
 import Container from '../../components/Container/Container'
 import Title from '../../components/Title/Title'
 import Button from '../../components/Button/Button'
@@ -6,7 +8,16 @@ import TextInput from '../../components/TextInput/TextInput'
 import avatar from '../../assets/default_user.jpg'
 import classes from './Identify.module.css'
 
-const Identify = () => {
+const Identify = (props) => {
+    const [error, setError] = useState(false)
+    const history = useHistory()
+
+    const errorMessage = error ? <span className={classes.error}>You need a name in order to play!</span> : undefined
+
+    if(error && props.name) {
+        setError(false)
+    }
+
     return <Container>
         <img
             src={avatar}
@@ -18,11 +29,35 @@ const Identify = () => {
                 What is your player name?
             </Title>
         </div>
-        <TextInput></TextInput>
-        <Button>
+        <TextInput
+            change={props.changeName}
+            value={props.name}
+        ></TextInput>
+        {errorMessage}
+        <Button
+            click={() => {
+                if (!props.name) {
+                    setError(true)
+                } else {
+                    history.push('/game')
+                }
+            }}
+        >
             PLAY
         </Button>
     </Container>
 }
 
-export default Identify
+const mapStateToProps = state => {
+    return {
+        name: state.user.name
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        changeName: (name) => dispatch({ type: 'CHANGE_NAME', name })
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Identify)
