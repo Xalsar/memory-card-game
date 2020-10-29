49dd6a42f4d51dd47cbdce06ee5fb03078ac1978
Xalsar <xavicarreragimbert@gmail.com>
2020-10-29 11:38:26 +0100

    Create Container component

---
+++ src/components/Container/Container.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import classes from './Container.module.css'
+
+const Container = (props) => {
+    return <main className={classes.container}>
+        {props.children}
+    </main>
+}
+
+export default Container
