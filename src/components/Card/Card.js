c3bc772977ee1675cf7a0ab73baf1e7b66614e48
Xalsar <xavicarreragimbert@gmail.com>
2020-10-31 19:34:56 +0100

    Make Card style class based

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,24 +2,25 @@ import React from 'react'
 import classes from './Card.module.css'
 
 const Card = (props) => {
-    if (props.hidden) {
-        return <div
-            className={[classes.card, classes.hidden].join(' ')}
-            onClick={props.click}
-        >
-            <h2>?</h2>
-        </div>
+    const activeClasses = [classes.card]
 
-    } else {
-        return <div
-            className={classes.card}
-            style={{
-                backgroundColor: `var(${props.color})`,
-                opacity: props.active ? 0.3 : 1
-            }}
-        >
-        </div>
+    if (props.hidden) {
+        activeClasses.push(classes.hidden)
     }
+
+    if(props.active) {
+        activeClasses.push(classes.active)
+    }
+
+    return <div
+        className={activeClasses.join(' ')}
+        style={{
+            backgroundColor: `var(${props.color})`
+        }}
+        onClick={props.hidden ? props.click : undefined}
+    >
+        {props.hidden ? <h2>?</h2> : undefined}
+    </div>
 }
 
 export default Card
