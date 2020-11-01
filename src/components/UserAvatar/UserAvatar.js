f65ce407a8027096809111ac407f19d4ea0e9f46
Xalsar <xavicarreragimbert@gmail.com>
2020-11-01 18:38:42 +0100

    Add change user to UserAvatar

--- src/components/UserAvatar/UserAvatar.js
+++ src/components/UserAvatar/UserAvatar.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import avatar from '../../assets/default_user.jpg'
 import classes from './UserAvatar.module.css'
 
 const UserAvatar = (props) => (
     <div className={classes.avatar}>
-        <img src={avatar} alt="User"/>
-        <h3>
-            {props.userName}
-        </h3>
+        <img src={avatar} alt="User" />
+        <div className={classes.text}>
+            <h3>
+                {props.userName}
+            </h3>
+            <Link to="/">
+                Change user
+            </Link>
+        </div>
     </div>
 )
 
