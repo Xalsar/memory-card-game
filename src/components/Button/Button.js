867fb529cbfee61bb77ba51a975569e02696fa0f
Xalsar <xavicarreragimbert@gmail.com>
2020-11-01 11:20:58 +0100

    Redesign Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,12 +2,15 @@ import React from 'react'
 import classes from './Button.module.css'
 
 const Button = (props) => (
-    <button
-        className={classes.button}
-        onClick={props.click}
-    >
-        {props.children}
-    </button>
+    <div className={classes.buttonContainer}>
+        <button
+            className={classes.button}
+            onClick={props.click}
+            type={props.type}
+        >
+            {props.children}
+        </button>
+    </div>
 )
 
 export default Button
