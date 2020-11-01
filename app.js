48a68c01674226206b56eaf62c8635ef3343c551
Xalsar <xavicarreragimbert@gmail.com>
2020-11-01 23:41:52 +0100

    =Add bodyParser and route to React app

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ require('./db/mongoose')
 const cors = require('cors')
 const userRouter = require('./routers/user')
 const scoreRouter = require('./routers/score')
+const bodyParser = require('body-parser');
 
 const app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json())
 app.use(cors())
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const app = require('./app')
-const path = require('path');
 const port = process.env.PORT
+const express = require('express')
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Handle React routing, return all requests to React app
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+})
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
